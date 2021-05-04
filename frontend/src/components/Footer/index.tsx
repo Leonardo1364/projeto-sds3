e8334d18a8531c2a36e1f66ad86411182f29876b
Leonardo Mota <leo_lmota@hotmail.com>
2021-05-03 21:17:22 -0300

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Leonardo1364" target="_blank" rel="noreferrer">Leonardo Mota</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/leonardoolima_" target="_blank" rel="noreferrer">@leonardoolima_</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
