09a9d4965b8ac8ac3e84337d1094bec680fa6929
Leonardo Mota <leo_lmota@hotmail.com>
2021-05-07 21:44:50 -0300

    Routes

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/Leonardo1364" target="_blank" rel="noreferrer">Leonardo Mota</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/leonardoolima_" target="_blank" rel="noreferrer">@leonardoolima_</a></small></p>
+      Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/leonardo-mota-647017131/" target="_blank" rel="noreferrer">@leonardo-mota</a></small></p>
             </div>
         </footer>
     );
