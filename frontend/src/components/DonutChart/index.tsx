fca4ff65c4462721a1ac886cae65cac05b85e8c6
Leonardo Mota <leo_lmota@hotmail.com>
2021-05-07 23:52:15 -0300

    DataTable integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -27,6 +27,7 @@ const DonutChart = () => {
 
 
     /*
+    Testagem dos dados
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
